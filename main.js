8be0226c00afa413c110d3a70958fc6e38e8cc22
Navindu Dananga <41588881+krypto-i9@users.noreply.github.com>
2020-07-19 21:38:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
-function calc(matrix, n){
-    for(let i = 0; i < matrix.length; i++){
-	for(let j = 0; j < matrix[i].length; j++){
-	    let oldV = matrix[i][j];
-	    let newV = matrix[i][n] + matrix[n][j];
-	    matrix[i][j] = (newV < oldV) ? newV : oldV; 
+class FloydWarshallAlgorithm{
+    calcOne(matrix, n){
+	for(let i = 0; i < matrix.length; i++){
+	    for(let j = 0; j < matrix[i].length; j++){
+		let oldV = matrix[i][j];
+		let newV = matrix[i][n] + matrix[n][j];
+		matrix[i][j] = (newV < oldV) ? newV : oldV; 
+	    }
 	}
-      }
-    return matrix;
+	return matrix;
+    }
+
+    run(matrix, args = {n : 0, detailed : false}){
+	if(args.n === matrix.length - 1) {
+	    if(args.detailed){
+		console.log("--------------------------------------");
+		console.log("[*] D" + (args.n + 1) + " Matrix: (final output)");
+		console.log("--------------------------------------");
+		console.log(matrix);
+	    }
+	    return matrix;
+	}
+	let d = this.calcOne(matrix, args.n);
+	if(args.detailed){
+	    console.log("[*] D" + (args.n + 1) + " Matrix:");
+	    console.log("--------------------------------------");
+	    console.log(d);
+	}
+	return this.run(d, {n : args.n + 1, detailed: args.detailed});   
+    }
 }
 
-function run(matrix, n = 0, detailed = false){
-    if(n === matrix.length) {
-	if(detailed){
-	    console.log("--------------------------------------");
-	    console.log("           [*] Final Result           ");
-	    console.log("--------------------------------------");
-	}
-	return matrix
-    }
-    let d = calc(matrix, n);
-    if(detailed){
-	console.log("[*] D" + (n + 1) + " Matrix:");
-	console.log("--------------------------------------");
-	console.log(d);
-    }
-    return run(d, n + 1, detailed);
-    
-}
 
-// run example
-const inf = Infinity;
-let matrix = [[0, 3, 8, inf, -4], [inf, 0, inf, 1, 7], [inf, 4, 0, inf, inf], [2, inf, -5, 0, inf], [inf, inf, inf, 6, 0]];
+// Run example
+// const inf = Infinity;
+// let matrix = [[0, 3, 8, inf, -4], [inf, 0, inf, 1, 7], [inf, 4, 0, inf, inf], [2, inf, -5, 0, inf], [inf, inf, inf, 6, 0]];
 
-console.log(run(matrix, 0, true));
+// let fwa = new FloydWarshallAlgorithm;
+// fwa.run(matrix, {n : 0, detailed : true});
